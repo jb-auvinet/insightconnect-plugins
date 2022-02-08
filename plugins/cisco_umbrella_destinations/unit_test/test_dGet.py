0f0b6677c14e0745ce356ef7fbacadc72ecb454e
Conor McNally <conor_mcnally@rapid7.com>
2022-02-08 14:09:44 +0000

    Test dGet basic success completed

--- plugins/cisco_umbrella_destinations/unit_test/test_dGet.py
+++ plugins/cisco_umbrella_destinations/unit_test/test_dGet.py
@@ -1,24 +1,60 @@
 import sys
 import os
-sys.path.append(os.path.abspath('../'))
 
-from unittest import TestCase
+sys.path.append(os.path.abspath("../"))
+
+from unittest import TestCase, mock
 from icon_cisco_umbrella_destinations.connection.connection import Connection
 from icon_cisco_umbrella_destinations.actions.dGet import DGet
+from icon_cisco_umbrella_destinations.actions.dGet.schema import Input
 import json
 import logging
 
+from unit_test.mock import STUB_CONNECTION, mock_request_200, mocked_request, STUB_DESTINATION_LIST_ID
+
 
 class TestDGet(TestCase):
-    def test_dGet(self):
-        """
-        DO NOT USE PRODUCTION/SENSITIVE DATA FOR UNIT TESTS
+    def setUp(self) -> None:
+        self.connection = Connection()
+        self.connection.logger = logging.getLogger("Connection logger")
+        self.connection.connect(STUB_CONNECTION)
 
-        TODO: Implement test cases here
+        self.action = DGet()
+        self.action.connection = self.connection
+        self.action.logger = logging.getLogger("Action logger")
 
-        For information on mocking and unit testing please go here:
+        self.params = {"destination_list_id": STUB_DESTINATION_LIST_ID}
 
-        https://docs.google.com/document/d/1PifePDG1-mBcmNYE8dULwGxJimiRBrax5BIDG_0TFQI/edit?usp=sharing
-        """
-
-        self.fail("Unimplemented Test Case")
+    @mock.patch("requests.request", side_effect=mock_request_200)
+    def test_destination_get_success(self, mock_get):
+        response = self.action.run({Input.DESTINATIONLISTID: STUB_DESTINATION_LIST_ID})
+        expected_response = {
+            "success": {
+                "status": {"code": 200, "text": "OK"},
+                "meta": {"page": 1, "limit": 100, "total": 3},
+                "data": [
+                    {
+                        "id": "6672",
+                        "destination": "google.co.uk",
+                        "type": "domain",
+                        "comment": "comment",
+                        "createdAt": "2022-02-02 14:04:29",
+                    },
+                    {
+                        "id": "183128",
+                        "destination": "stackoverflow.com",
+                        "type": "domain",
+                        "comment": "another",
+                        "createdAt": "2022-01-31 17:07:15",
+                    },
+                    {
+                        "id": "42882950",
+                        "destination": "enteradestination",
+                        "type": "domain",
+                        "comment": "EnterAComment",
+                        "createdAt": "2022-01-31 17:06:28",
+                    },
+                ],
+            }
+        }
+        self.assertEqual(response, expected_response)
