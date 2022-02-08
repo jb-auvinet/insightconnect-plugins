c554c3f6f77f854039f2cc6ab6b8df81a7a30eef
Conor McNally <conor_mcnally@rapid7.com>
2022-02-08 13:54:23 +0000

    dlGet Test success complete

--- plugins/cisco_umbrella_destinations/unit_test/test_dlGet.py
+++ plugins/cisco_umbrella_destinations/unit_test/test_dlGet.py
@@ -4,18 +4,13 @@ import os
 sys.path.append(os.path.abspath("../"))
 
 from unittest import TestCase, mock
-from icon_cisco_umbrella_destinations.actions.dlGet.schema import Output
 from icon_cisco_umbrella_destinations.connection.connection import Connection
-from icon_cisco_umbrella_destinations.connection.schema import Input
 from icon_cisco_umbrella_destinations.actions.dlGet import DlGet
+from icon_cisco_umbrella_destinations.actions.dlGet.schema import Input
 import json
 import logging
 
-from ..unit_test.mock import (
-    STUB_CONNECTION,
-    mock_request_200,
-    mocked_request
-)
+from unit_test.mock import STUB_CONNECTION, mock_request_200, mocked_request, STUB_DESTINATION_LIST_ID
 
 
 class TestDlGet(TestCase):
@@ -28,27 +23,28 @@ class TestDlGet(TestCase):
         self.action.connection = self.connection
         self.action.logger = logging.getLogger("Action logger")
 
-    # def test_dlGet(self):
+        self.params = {"destination_list_id": STUB_DESTINATION_LIST_ID}
 
-
-    # @mock.patch("requests.request", side_effect=mock_request_200)
-    def test_destination_list_get_success(self):
-        mocked_request(mock_request_200)
-        response = self.action.run()
+    @mock.patch("requests.request", side_effect=mock_request_200)
+    def test_destination_list_get_success(self, mock_get):
+        response = self.action.run({Input.DESTINATIONLISTID: STUB_DESTINATION_LIST_ID})
         expected_response = {
-            "id": 15755711,
-            "organizationId": 2372338,
-            "access": "allow",
-            "isGlobal": False,
-            "name": "CreateListTest",
-            "thirdpartyCategoryId": None,
-            "createdAt": "2022-01-28T16:03:36+0000",
-            "modifiedAt": "2022-02-02T14:04:29+0000",
-            "isMspDefault": False,
-            "markedForDeletion": False,
-            "bundleTypeId": 1,
-            "meta": {
-                "destinationCount": 5
+            "success": {
+                "status": {"code": 200, "text": "OK"},
+                "data": {
+                    "id": 15755711,
+                    "organizationId": 2372338,
+                    "access": "allow",
+                    "isGlobal": False,
+                    "name": "CreateListTest",
+                    "thirdpartyCategoryId": None,
+                    "createdAt": "2022-01-28T16:03:36+0000",
+                    "modifiedAt": "2022-02-04T14:59:22+0000",
+                    "isMspDefault": False,
+                    "markedForDeletion": False,
+                    "bundleTypeId": 1,
+                    "meta": {"destinationCount": 3},
+                },
             }
         }
         self.assertEqual(response, expected_response)
