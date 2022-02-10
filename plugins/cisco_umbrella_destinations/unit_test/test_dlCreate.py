57da38d620c49e57c2a129b636cd4eb112cdb3c8
Conor McNally <conor_mcnally@rapid7.com>
2022-02-10 11:07:35 +0000

    Save point #3

--- plugins/cisco_umbrella_destinations/unit_test/test_dlCreate.py
+++ plugins/cisco_umbrella_destinations/unit_test/test_dlCreate.py
@@ -3,23 +3,32 @@ import os
 
 sys.path.append(os.path.abspath("../"))
 
-from unittest import TestCase
+from unittest import TestCase, mock
 from icon_cisco_umbrella_destinations.connection.connection import Connection
-from icon_cisco_umbrella_destinations.actions.dlCreate import DlCreate
+from ..icon_cisco_umbrella_destinations.actions.dlCreate import DlCreate
+from icon_cisco_umbrella_destinations.actions.dlCreate.schema import Input
 import json
 import logging
+from unit_test.mock import STUB_CONNECTION, mock_request_200, STUB_DESTINATION_LIST_ID
 
 
 class TestDlCreate(TestCase):
-    def test_dlCreate(self):
-        """
-        DO NOT USE PRODUCTION/SENSITIVE DATA FOR UNIT TESTS
+    def setUp(self) -> None:
+        self.connection = Connection()
+        self.connection.logger = logging.getLogger("Connection logger")
+        self.connection.connect(STUB_CONNECTION)
 
-        TODO: Implement test cases here
+        self.action = DlCreate()
+        self.action.connection = self.connection
+        self.action.logger = logging.getLogger("Action logger")
 
-        For information on mocking and unit testing please go here:
+        self.data = {
+            "access": "allow",
+            "isGlobal": True,
+            "name": "NewName",
+        }
 
-        https://docs.google.com/document/d/1PifePDG1-mBcmNYE8dULwGxJimiRBrax5BIDG_0TFQI/edit?usp=sharing
-        """
-
-        self.fail("Unimplemented Test Case")
+    @mock.patch("requests.request", side_effect=mock_request_200)
+    def test_successful(self, mock_post):
+        response = self.action.run()
+        expected_response = []
--- plugins/cisco_umbrella_destinations/unit_test/test_dlDelete.py
+++ plugins/cisco_umbrella_destinations/unit_test/test_dlDelete.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath("../"))
 from unittest import TestCase, mock
 from icon_cisco_umbrella_destinations.connection.connection import Connection
 from icon_cisco_umbrella_destinations.actions.dlDelete import DlDelete
+from icon_cisco_umbrella_destinations.actions.dlDelete.schema import Input
 import json
 import logging
 from unit_test.mock import STUB_CONNECTION, mock_request_200, STUB_DESTINATION_LIST_ID
@@ -23,7 +24,7 @@ class TestDlDelete(TestCase):
 
     @mock.patch("requests.request", side_effect=mock_request_200)
     def test_successful(self, mock_delete):
-        response = self.action.run()
-        expected_response = {"success": {"status": {"code": 200, "text": "OK"}, "data": []}}
+        response = self.action.run({Input.DESTINATIONLISTID: STUB_DESTINATION_LIST_ID})
+        expected_response = {"success": []}
 
         self.assertEqual(response, expected_response)
