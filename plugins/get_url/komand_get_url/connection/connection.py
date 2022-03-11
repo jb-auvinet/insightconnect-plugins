cc34f7c55fb68a52c76937474df8c18c79566e9c
blaxminarayan-r7 <93721080+blaxminarayan-r7@users.noreply.github.com>
2022-03-11 07:37:37 -0500

    Revert "[MC-973] Get_url update sdk to version 4. Update unit tests after update SDK (#1165)"
    This reverts commit 530d0c9d1d8d6d3946f57fad85a701d9440fab75.

--- plugins/get_url/komand_get_url/connection/connection.py
+++ plugins/get_url/komand_get_url/connection/connection.py
@@ -1,18 +1,17 @@
-import insightconnect_plugin_runtime
+import komand
 from .schema import ConnectionSchema
 
 # Custom imports below
 
 
-class Connection(insightconnect_plugin_runtime.Connection):
+class Connection(komand.Connection):
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
 
-    # pylint: disable=unused-argument
-    def connect(self, params={}):
+    def connect(self, params):
         pass
 
     def test(self, params={}):
         url = "https://www.google.com"
-        insightconnect_plugin_runtime.helper.check_url(url)
+        komand.helper.check_url(url)
         return {}
